9aa0bfc5b12a2d60daad8360b9e54e1717e56e0b
AgustinOsses <ossesagus@gmail.com>
2021-06-15 15:36:09 -0300

    Repaso: Cache con Network Fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const STATIC_CACHE = 'static-v1';
+importScripts('js/sw-utils.js')
+
+const STATIC_CACHE = 'static-v2';
 const DINAMIC_CACHE = 'dinamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -13,6 +15,7 @@ const APP_SHELL = [
   'img/avatars/thor.jpg',
   'img/avatars/wolverine.jpg',
   'js/app.js',
+  'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -44,3 +47,20 @@ self.addEventListener('active', (e) => {
 		});
 	});
 });
+
+self.addEventListener('fetch', e => {
+  const respuesta = caches.match(e.request).then(res => {
+    if(res){
+      return res
+    }
+    else {
+      return fetch(e.request).then( newRes => {
+        return actualizaCacheDinamico(DINAMIC_CACHE, e.request, newRes);
+      })
+    }
+    console.log(res)
+  })
+	e.waitUntil(respuesta)
+
+
+})
